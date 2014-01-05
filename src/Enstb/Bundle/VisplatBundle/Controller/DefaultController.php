3075b6dddf74b70fb090d55bc1db49fbd27be357
Varayut Lerdkanlayanawat <l.varayut@gmail.com>
2014-01-05 22:52:59 +0100

    [Add] Secure the administrator page

--- src/Enstb/Bundle/VisplatBundle/Controller/DefaultController.php
+++ src/Enstb/Bundle/VisplatBundle/Controller/DefaultController.php
@@ -3,11 +3,32 @@
 namespace Enstb\Bundle\VisplatBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('EnstbVisplatBundle:Graph:status.html.twig');
+    }
+
+    public function loginAction()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render('EnstbVisplatBundle:Login:login.html.twig', array(
+            // last username entered by the user
+            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'error'         => $error,
+        ));
     }
 }
